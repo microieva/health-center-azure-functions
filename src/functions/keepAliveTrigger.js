280f90b1274fb648d719156e183f604e28ef5fe9
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-02-08 16:44:17 +0200

    initial

---
+++ src/functions/keepAliveTrigger.js
@@ -0,0 +1,30 @@
+const { app } = require('@azure/functions');
+const sql = require('mssql');
+
+app.timer('triggerKeepAlive', {
+    schedule: '0 */15 7-23 * * *',
+    handler: async (myTimer, context) => {
+        context.log('Timer function processed request');
+
+        const config = {
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            server: process.env.DB_SERVER,
+            database: process.env.DB_DATABASE,
+            options: {
+                encrypt: true, 
+                enableArithAbort: true
+            }
+        };
+
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request().query('SELECT 1');
+            context.log(`Keep Alive Query result: ${result.recordset}`);
+            sql.close();
+        } catch (err) {
+            context.log('Error running query:', err);
+            sql.close();
+        }
+    }
+});
